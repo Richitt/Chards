eb95e6f4c25bfbb67b0591a3f3589c7870251ad3
Richard Wang <richitt10@gmail.com>
2021-07-08 00:54:17 -0400

    added the start of targeting

--- Assets/Scripts/UI/BattleManager.cs
+++ Assets/Scripts/UI/BattleManager.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
+    private GameObject selectedTarget;
+    public GameObject[] enemies;
+    private int selector;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        selector = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       if(Input.GetKeyDown(KeyCode.A)){
+           selectTarget();
+       }
+    }
+
+    private void selectTarget(){
+        selector++;
+        if (enemies.Length == 0){
+            enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        }
+        selectedTarget = enemies[selector];
+        Debug.Log(selectedTarget.name);
     }
 }
