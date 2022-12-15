0f39d2f3aab265f93067642e82459072a36a39ed
Richitt <richitt10@gmail.com>
2022-12-14 22:14:06 -0500

    updating pos back and hp depletion

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -74,6 +74,7 @@ public class Player : MonoBehaviour
         //}
         selectedTarget = enemies[selector];
         Debug.Log(selectedTarget.name);
+        Debug.Log("AAAAYOOOO");
     }
 
     
