a6a2aaefaa8d4b77557922341f2843c6e025d4f6
Richitt <richitt10@gmail.com>
2023-03-31 03:28:50 -0400

    added a deck and card start, also added animations for them

---
+++ Assets/Scripts/UI/Deck.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    public GameObject[] cards;
+    private int counter = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject card in cards){
+            var p = Instantiate(card);
+            p.transform.parent = gameObject.transform;
+            p.transform.position = gameObject.transform.position - new Vector3(3.6f,0,0);
+            var x = p.GetComponent<Card>();
+            x.setDistance(100 * counter);
+            counter++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
